8e1e78f2be9ac4cd1d05888acb8468a09e54ecc4
zhenyarudenok <57105830+yavgen999@users.noreply.github.com>
2020-11-12 18:48:39 +0300

    Update Product.tsx

--- src/components/MainPanel/Products/Product.tsx
+++ src/components/MainPanel/Products/Product.tsx
@@ -8,10 +8,15 @@ import {
   Div,
   Button,
   HorizontalScroll,
-  Title
+  Title,
+  Link
 } from "@vkontakte/vkui";
 
 import Icon16MoreVertical from '@vkontakte/icons/dist/16/more_vertical';
+import Icon24PenOutline from '@vkontakte/icons/dist/24/pen_outline';
+import Icon20ArticleBoxOutline from '@vkontakte/icons/dist/20/article_box_outline';
+// import Icon20CheckCircleOutline from '@vkontakte/icons/dist/20/check_circle_outline'; Почему то не может импортировать
+// import Icon20ErrorCircleOutline from '@vkontakte/icons/dist/20/error_circle_outline';
 
 interface IProps {}
 
@@ -43,6 +48,18 @@ export default class extends React.Component<IProps, IState> {
     <Div>
     <Title level="3" weight="regular" >Сервис с кулинарными рецептами.</Title>
     </Div>
+    </Group>
+
+    <Group>
+      <Header aside={<Link>Показать все</Link>}>
+        Версии
+      </Header>
+      <Header aside={<Link>тут иконки</Link>}>
+        9.9.9.9.9 <Link><Icon24PenOutline height={16} width={16}/> Изменить</Link>
+      </Header>
+      <Div>
+        <Title level="3" weight="regular">-текст<br/>-текст<br/>-текст<br/>-текст-текст<br/>-текст-текст<br/>-текст</Title>
+      </Div>
     </Group>
 
     </div>
